2051b722a18254b151de084b74c847714dd137f2
shubham <deore567@mail.com>
2023-01-18 10:52:51 +0530

    minor changes

--- auth/auth/settings.py
+++ auth/auth/settings.py
@@ -129,8 +129,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "deore567@gmail.com"
-EMAIL_HOST_PASSWORD = "hdifajcxeebmuchb"
+EMAIL_HOST_USER = "your email here"
+EMAIL_HOST_PASSWORD = "your_password here"
 
 
 LOGIN_EXEMPT_URLS = (
